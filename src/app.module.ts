f1473b274e2ea1fd4f422fcf07a1de070f0b076b
Gabriel Rocha <gabrielroc2112@gmail.com>
2024-08-29 01:52:32 +0000

    aula 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    driver: "postgres",
+    database: "todo",
+    host: "ep-red-queen-a5q7tbgv.us-east-2.aws.neon.tech",
+    username: "todo_owner",
+    password: "IsejV2W4SZPv",
+    ssl: true,
+    synchronize: true,
+    autoLoadEntities: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// postgresql://todo_owner:IsejV2W4SZPv@ep-red-queen-a5q7tbgv.us-east-2.aws.neon.tech/todo?sslmode=require
